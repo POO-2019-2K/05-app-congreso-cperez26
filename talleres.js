ab9eb5625203098ce97bd70b227bb79b9cc38ea2
cperez26 <42821682+cperez26@users.noreply.github.com>
2019-05-09 12:18:28 -0500

    segundo commit

--- talleres.js
+++ talleres.js
@@ -15,4 +15,34 @@ export default class ListaTalleres {
             this._showInTable(new Taller(e));
         });
     }
+
+    _showInTable(taller) {
+        let row = this._tabla.insertRow(-1);
+
+        let cellNombre = row.insertCell(0);
+        let cellFechaInicio = row.insertCell(1);
+        let cellFechaFinal = row.insertCell(2);
+        let cellHoras = row.insertCell(3);
+        let cellLugares = row.insertCell(4);
+
+        cellNombre.innerHTML = taller.nombre;
+        cellFechaInicio.innerHTML = taller.fechaInicio;
+        cellFechaFinal.innerHTML = taller.fechaFinal;
+        cellHoras.innerHTML = taller.horas;
+        cellLugares.innerHTML = taller.lugares;
+
+        let objTaller = {
+            nombre: taller.nombre,
+            fechaInicio: taller.fechaInicio,
+            fechaFinal: taller.fechaFinal,
+            horas: taller.horas,
+            lugares: taller.lugares
+        }
+        this._talleres.push(objTaller);
+    }
+    addTaller(taller) {
+        this._showInTable(taller);
+        localStorage.setItem("talleres", JSON.stringify(this._talleres));
+        localStorage.setItem("talleres", JSON.stringify(this._talleres));
+    }
 }
