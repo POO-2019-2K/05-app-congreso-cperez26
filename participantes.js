72f2ca46c7bf7bc31d65513d4a01a298555554bc
cperez26 <42821682+cperez26@users.noreply.github.com>
2019-05-14 18:56:48 -0500

    segundo commit (correccion)

--- participantes.js
+++ participantes.js
@@ -15,4 +15,30 @@ export default class Participantes {
             this._showInTable2(new ParticipantesT)
         });
     }
+    _showInTableP(participante) {
+        let row = this._tablaPinsertRow(-1);
+
+        let cellNombreP = row.insertCell(0);
+        let cellEmail = row.insertCell(1);
+        let cellFechaN = row.insertCell(2);
+        let cellNombreT = row.insertCell(3);
+
+        cellNombreP.innerHTML = participante.cellNombreP;
+        cellEmail.innerHTML = participante.email;
+        cellFechaN.innerHTML = participante.fechaN;
+        cellNombreT.innerHTML = participante.nombreT;
+
+        let objParticipante = {
+            cellNombreP: participante.cellNombreP,
+            email: participante.email,
+            fechaN: participante.fechaN,
+            nombreT: participante.nombreT,
+        }
+        this._participante.push(objParticipante);
+    }
+    addParticipantes(participante) {
+        this._showInTableP(participante);
+        localStorage.setItem("participantes", JSON.stringify(this._participantes));
+        localStorage.setItem("participantes", JSON.stringify(this._participantes));
+    }
 }
